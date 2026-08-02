1f7a3926ca8c2a54d4465ce008c09fabae0fde91
finadamayanti <alfina.dmayanti26@gmail.com>
2026-08-02 15:06:27 +0700

    Menambahkan tugas HTML Pertemuan 1-4

---
+++ coding/objek.js
@@ -0,0 +1,12 @@
+//Deklarasi Odject
+const karakter = {
+    nama: 'Dora',
+    level: 7,
+    hp: 100,
+    senjata: 'pistol'
+};
+//output
+console.log(karakter.nama);
+console.log(karakter.level);
+console.log(karakter.hp);
+console.log(karakter.senjata);
